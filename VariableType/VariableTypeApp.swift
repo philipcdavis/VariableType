a50f5d25850fc06b2d2878ad0e60798b0e795bc9
Philip Davis <philipcdavis11@gmail.com>
2022-06-09 09:09:09 -0700

    Initial Commit

---
+++ VariableType/VariableTypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  VariableTypeApp.swift
+//  VariableType
+//
+//  Created by Philip Davis on 6/9/22.
+//
+
+import SwiftUI
+
+@main
+struct VariableTypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
